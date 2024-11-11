ebf4dc232de7ba5a16a6141254a46e09f53b86f8
keserizzet <izzetfurkankeserwtk@gmail.com>
2024-11-11 19:28:42 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -8,8 +8,9 @@ function makeTransaction(quantity, pricePerDroid, customerCredits){
     }
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "15000 kredi değerinde 5 droid sipariş ettiniz!" 
-console.log(makeTransaction(3, 1000, 15000)); // "3000 kredi değerinde 3 droid sipariş ettiniz!" 
-console.log(makeTransaction(10, 5000, 8000)); // "Yetersiz bakiye!" 
-console.log(makeTransaction(8, 2000, 10000)); // "Yetersiz bakiye!" 
-console.log(makeTransaction(10, 500, 5000)); // "5000 kredi değerinde 10 droid sipariş ettiniz!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
